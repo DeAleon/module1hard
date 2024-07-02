8c987f75e80d4134533d41adddb8130eba0118e5
DeAleon <donnotel@mail.ru>
2024-07-02 15:11:13 +0300

    Add files via upload

--- module1hard.py
+++ module1hard.py
@@ -1,12 +1,19 @@
 grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
 students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
 students_set = set((students))
-grades[0] = sum(grades[0])/len(grades[0]) #Пробовал по разному не получилось приминить формулу ко всему списку сразу(
-grades[1] = sum(grades[1])/len(grades[1])
-grades[2] = sum(grades[2])/len(grades[2])
-grades[3]= sum(grades[3])/len(grades[3])
-grades[4] = sum(grades[4])/len(grades[4])
+grades_m = []
+for num in grades:
+    s= sum(num)/len(num)
+    grades_m.append(s)
+
+#grades[0] = sum(grades[0])/len(grades[0]) #Пробовал по разному не получилось приминить формулу ко всему списку сразу(
+#grades[1] = sum(grades[1])/len(grades[1])
+#grades[2] = sum(grades[2])/len(grades[2])
+#grades[3]= sum(grades[3])/len(grades[3])
+#grades[4] = sum(grades[4])/len(grades[4])
 #print(grades)
 
-grades_students = dict(zip(sorted(students_set), grades))
+grades_students = dict(zip(sorted(students_set), grades_m))
 print(grades_students)
+
+
